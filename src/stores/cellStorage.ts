71093f2f15c7f071bfb446f8c4753e2297f6c7bf
gretmn102 <erlgreywatson@gmail.com>
2026-06-10 15:17:16 +0300

    refactor(CellStorage.create): convert to object

--- src/stores/cellStorage.ts
+++ src/stores/cellStorage.ts
@@ -5,33 +5,93 @@ export type CellStorage = CellData[]
 
 export namespace CellStorage {
   export function create(): CellStorage {
-    const items: [string, string | undefined][] = [
-      ["Аперо", "./items/apero.png"],
-      ["Гильдия авторов", "./items/authors-guild.png"],
-      ["Axma", "./items/axma.png"],
-      ["Choices", "./items/choices.png"],
-      ["FireURQ", "./items/furq.png"],
-      ["Книги-игры", "./items/gamebook.png"],
-      ["Instead", "./items/instead.png"],
-      ["Instory", "./items/instory.png"],
-      ["Meander", "./items/meander.png"],
-      ["MUD'ы", "./items/mud.png"],
-      ["MURQ", "./items/murq.png"],
-      ["Парсеры", "./items/parsers.png"],
-      ["QSP", "./items/qsp.png"],
-      ["Квестбук", "./items/questbook.png"],
-      ["RenPy", "./items/renpy.png"],
-      ["Rinform", "./items/rinform.png"],
-      ["Rtads", "./items/rtads.png"],
-      ["Storymaze2.0", "./items/storymaze2.0.png"],
-      ["ТК: играй, пиши", "./items/tk.png"],
-      ["Twine", "./items/twine.png"],
-      ["Визуальные новеллы", "./items/vn.png"],
+    const items: CellStorage = [
+      {
+        description: "Аперо",
+        imageSrc: "./items/apero.png",
+      },
+      {
+        description: "Гильдия авторов",
+        imageSrc: "./items/authors-guild.png",
+      },
+      {
+        description: "Axma",
+        imageSrc: "./items/axma.png",
+      },
+      {
+        description: "Choices",
+        imageSrc: "./items/choices.png",
+      },
+      {
+        description: "FireURQ",
+        imageSrc: "./items/furq.png",
+      },
+      {
+        description: "Книги-игры",
+        imageSrc: "./items/gamebook.png",
+      },
+      {
+        description: "Instead",
+        imageSrc: "./items/instead.png",
+      },
+      {
+        description: "Instory",
+        imageSrc: "./items/instory.png",
+      },
+      {
+        description: "Meander",
+        imageSrc: "./items/meander.png",
+      },
+      {
+        description: "MUD'ы",
+        imageSrc: "./items/mud.png",
+      },
+      {
+        description: "MURQ",
+        imageSrc: "./items/murq.png",
+      },
+      {
+        description: "Парсеры",
+        imageSrc: "./items/parsers.png",
+      },
+      {
+        description: "QSP",
+        imageSrc: "./items/qsp.png",
+      },
+      {
+        description: "Квестбук",
+        imageSrc: "./items/questbook.png",
+      },
+      {
+        description: "RenPy",
+        imageSrc: "./items/renpy.png",
+      },
+      {
+        description: "Rinform",
+        imageSrc: "./items/rinform.png",
+      },
+      {
+        description: "Rtads",
+        imageSrc: "./items/rtads.png",
+      },
+      {
+        description: "Storymaze2.0",
+        imageSrc: "./items/storymaze2.0.png",
+      },
+      {
+        description: "ТК: играй, пиши",
+        imageSrc: "./items/tk.png",
+      },
+      {
+        description: "Twine",
+        imageSrc: "./items/twine.png",
+      },
+      {
+        description: "Визуальные новеллы",
+        imageSrc: "./items/vn.png",
+      }
     ]
-    return items.map(([description, imageSrc]) => ({
-      description,
-      imageSrc,
-    }))
+    return items
   }
 
   export function updateCell(
