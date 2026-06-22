986130cba0caafb41a63cb6085cf5832bbf72536
gretmn102 <erlgreywatson@gmail.com>
2026-06-22 13:53:50 +0300

    wip: обернуть в узел с `p-4`

--- src/components/Modal.svelte
+++ src/components/Modal.svelte
@@ -33,50 +33,53 @@
       on:click={close}
     />
 
-    <!-- Modal panel -->
     <div class={concat([
-      "relative",
-      "bg-white",
-      "dark:bg-gray-800",
-      "rounded-lg",
-      "shadow-lg",
+      "p-4",
+      "max-h-[calc(var(--vh,_1vh)_*_100)]",
       "max-w-lg",
       "w-full",
-      "max-h-[calc(var(--vh,_1vh)_*_100)]",
-      "mx-4",
-      "p-6",
-      "z-10",
-      "outline-none",
       "flex",
-      "flex-col",
     ])}>
-      <div class="flex items-start justify-between space-x-4">
-        <h2 id="modal-title" class="text-lg font-semibold text-gray-800 dark:text-gray-100">{title}</h2>
-        <button
-          class="text-gray-500 hover:text-gray-700 dark:text-gray-300"
-          aria-label="Close"
-          on:click={close}
-        >
-          <!-- simple X -->
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-          </svg>
-        </button>
-      </div>
-
       <div class={concat([
-        "mt-2",
-        "text-sm",
-        "text-gray-600",
-        "dark:text-gray-300",
-        "overflow-y-auto",
+        "bg-white",
+        "dark:bg-gray-800",
+        "rounded-lg",
+        "shadow-lg",
+        "grow",
+        "p-6",
+        "z-10",
+        "outline-none",
+        "flex",
+        "flex-col",
       ])}>
-        <slot />
-      </div>
+        <div class="flex items-start justify-between space-x-4">
+          <h2 id="modal-title" class="text-lg font-semibold text-gray-800 dark:text-gray-100">{title}</h2>
+          <button
+            class="text-gray-500 hover:text-gray-700 dark:text-gray-300"
+            aria-label="Close"
+            on:click={close}
+          >
+            <!-- simple X -->
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+          </button>
+        </div>
 
-      <div class="mt-6 flex justify-end gap-2">
-        <slot name="secondary" />
-        <slot name="primary" />
+        <div class={concat([
+          "mt-2",
+          "text-sm",
+          "text-gray-600",
+          "dark:text-gray-300",
+          "overflow-y-auto",
+        ])}>
+          <slot />
+        </div>
+
+        <div class="mt-6 flex justify-end gap-2">
+          <slot name="secondary" />
+          <slot name="primary" />
+        </div>
       </div>
     </div>
   </div>
