504b6ae1414dc0a73c6676811c11bcfadf6ebd34
gretmn102 <erlgreywatson@gmail.com>
2026-06-23 10:09:59 +0300

    wip: heading

--- src/components/Markdown/Heading.svelte
+++ src/components/Markdown/Heading.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
   import { type Token, type Tokens } from "marked"
 
+  import Inline from "./Inline.svelte"
+
   export let token: Token
   let heading = token as Tokens.Heading
 </script>
 
-<pre>
-  Heading: {JSON.stringify(heading, undefined, 2)}
-</pre>
+<!-- todo: handle heading.depth -->
+<h1>
+  {#each heading.tokens as token}
+    <Inline token={token} />
+  {/each}
+</h1>
