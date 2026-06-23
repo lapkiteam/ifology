a76abad19bdd62e06cca9f37efecebc8f24617ed
Agent Lapki <treesortbubble@gmail.com>
2026-06-23 13:25:08 +0300

    feat(components/Markdown): handle html (#42)

---
+++ src/components/Markdown/inlines/Html.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import { type Token, type Tokens } from "marked"
+  import DOMPurify from "dompurify"
+
+  export let token: Token
+  let html = token as Tokens.HTML
+</script>
+
+{@html DOMPurify.sanitize(html.text)}
