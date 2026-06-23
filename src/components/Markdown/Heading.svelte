8543be55178b0807823ba2a7b47021e41fe2a30c
gretmn102 <erlgreywatson@gmail.com>
2026-06-23 10:43:15 +0300

    feat(heading): handle heading.depth

--- src/components/Markdown/Heading.svelte
+++ src/components/Markdown/Heading.svelte
@@ -7,7 +7,32 @@
   let heading = token as Tokens.Heading
 </script>
 
-<!-- todo: handle heading.depth -->
-<h1>
-  <Inlines tokens={heading.tokens} />
-</h1>
+{#if heading.depth === 1}
+  <h1>
+    <Inlines tokens={heading.tokens} />
+  </h1>
+{:else if heading.depth === 2}
+  <h2>
+    <Inlines tokens={heading.tokens} />
+  </h2>
+{:else if heading.depth === 3}
+  <h3>
+    <Inlines tokens={heading.tokens} />
+  </h3>
+{:else if heading.depth === 4}
+  <h4>
+    <Inlines tokens={heading.tokens} />
+  </h4>
+{:else if heading.depth === 5}
+  <h5>
+    <Inlines tokens={heading.tokens} />
+  </h5>
+{:else if heading.depth === 6}
+  <h6>
+    <Inlines tokens={heading.tokens} />
+  </h6>
+{:else}
+  <h6>
+    <Inlines tokens={heading.tokens} />
+  </h6>
+{/if}
