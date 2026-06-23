e3ff360c74e814d3826c601cb7a9597ea86d1d53
Agent Lapki <treesortbubble@gmail.com>
2026-06-23 11:36:25 +0300

    feat(components/Markdown/Blockquote): add style (#33)

--- src/components/Markdown/Blockquote.svelte
+++ src/components/Markdown/Blockquote.svelte
@@ -1,13 +1,28 @@
 <script lang="ts">
   import { type Token, type Tokens } from "marked"
 
+  import { concat } from "../../lib/utils"
   import Block from "./Block.svelte"
 
   export let token: Token
   const blockquote = token as Tokens.Blockquote
 </script>
 
-<blockquote>
+<blockquote class={concat([
+  "relative",
+  "overflow-hidden",
+  "px-4",
+  "py-2",
+  "bg-gray-200",
+  "dark:bg-gray-700",
+  "before:absolute",
+  "before:left-0",
+  "before:top-0",
+  "before:w-1",
+  "before:h-full",
+  "before:bg-gray-600",
+  "before:dark:bg-gray-600",
+])}>
   {#each blockquote.tokens as item}
     <Block token={item} />
   {/each}
