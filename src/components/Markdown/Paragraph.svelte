b45b4df94ae3c677efa0d711b208d2764e389673
gretmn102 <erlgreywatson@gmail.com>
2026-06-23 11:40:31 +0300

    wip: add `mb-2`

--- src/components/Markdown/Paragraph.svelte
+++ src/components/Markdown/Paragraph.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
   import { type Token, type Tokens } from "marked"
 
+  import { concat } from "../../lib/utils"
   import Inlines from "./Inlines.svelte"
 
   export let token: Token
   let paragraph = token as Tokens.Paragraph
 </script>
 
-<p>
+<p class={concat([
+  "mb-2"
+])}>
   <Inlines tokens={paragraph.tokens} />
 </p>
