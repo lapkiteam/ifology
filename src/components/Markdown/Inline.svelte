f0c537d229adc97297e6b2740c8836a0c45d26c1
gretmn102 <erlgreywatson@gmail.com>
2026-06-23 13:13:02 +0300

    feat(components/Markdown/Inline): handle html

--- src/components/Markdown/Inline.svelte
+++ src/components/Markdown/Inline.svelte
@@ -3,6 +3,7 @@
 
   import Text from "./inlines/Text.svelte"
   import Link from "./inlines/Link.svelte"
+  import Html from "./inlines/Html.svelte"
 
   export let token: Token
 </script>
@@ -11,6 +12,8 @@
   <Text token={token} />
 {:else if token.type === "link"}
   <Link token={token} />
+{:else if token.type === "html"}
+  <Html token={token} />
 {:else}
   <pre>{JSON.stringify(token, undefined, 2)}</pre>
 {/if}
---
+++ src/components/Markdown/inlines/Html.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { type Token, type Tokens } from "marked"
+
+  export let token: Token
+  let html = token as Tokens.HTML
+</script>
+
+{@html html.text}
