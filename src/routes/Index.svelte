807239e7e60d1c52ae6ad7c52bf4170fc94ad275
gretmn102 <erlgreywatson@gmail.com>
2026-06-26 12:09:52 +0300

    feat: add route system (#47)

---
+++ src/routes/Index.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import Router, { type RouteDefinition } from "svelte-spa-router"
+
+  import Home from "./Home.svelte"
+  import Items from "./Items.svelte"
+
+  export const routes: RouteDefinition = {
+    "/": Home,
+    "/items/:id": Items,
+  }
+</script>
+
+<Router routes={routes} />
